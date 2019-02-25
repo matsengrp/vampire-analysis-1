be2bad48f64ab54521cfd028dc440caae9c7f862
Erick Matsen <matsen@fredhutch.org>
2019-02-25 10:00:26 -0800

    script formatting.

--- scripts/select_subjects.py
+++ scripts/select_subjects.py
@@ -15,8 +15,10 @@ out_df['count'] = np.floor(df_lil.sum(axis=1))
 
 for n_subjects_to_select in [5, 10, 50, 100, 500]:
     print(f"Selecting {n_subjects_to_select} subjects...")
-    selected_subjects = np.random.choice(n_subjects, n_subjects_to_select, replace=False)
+    selected_subjects = np.random.choice(
+        n_subjects, n_subjects_to_select, replace=False)
     # np.floor used here just for type conversion
-    out_df[f'count_in_{n_subjects_to_select}'] = np.floor(df_lil[:, tuple(selected_subjects)].sum(axis=1))
+    out_df[f'count_in_{n_subjects_to_select}'] = np.floor(
+        df_lil[:, tuple(selected_subjects)].sum(axis=1))
 
 out_df.to_csv('output/hansen666.subject_subsampled.csv.bz2', compression='bz2')
